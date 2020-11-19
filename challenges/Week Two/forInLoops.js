ff75493352a7a4b43cba0b9ac46f8654599cea89
jesnadolski <nadolski.jessica@gmail.com>
2020-11-19 18:01:24 -0500

    next commit

--- challenges/Week Two/forInLoops.js
+++ challenges/Week Two/forInLoops.js
@@ -3,3 +3,93 @@
 For In Loops
 
 */
+
+
+//CLASS CHALLENGE - GET ON SLACK
+
+let fellowshipOfTheRing = {
+    members: {
+        Gandalf: 'Staff',
+        Frodo: 'Sting',
+        Sam: 'Lembas Bread',
+        Aragorn: 'Anduril',
+        Legolas: 'Bow of the Galadhrim',
+        Gimli: 'Walking Axe',
+        Pippin: 'Barrow Blades',
+        Merry: 'Barrow Blades',
+        Boromir: 'Horn of Gondor'
+    },
+    purpose: "To take the One Ring to Mordor, where it is to be \"cast into the fiery chasm from whence it came,\" in order for it to be destroyed and Sauron\'s power to come to an end.",
+    formed: '25 October, 3018',
+    dissolved: '26 February, 3019'
+}
+
+
+
+//BRONZE
+
+
+for (member in fellowshipOfTheRing.members){
+console.log(member);
+}
+
+//SILVER
+
+
+let fellowshipOfTheRing = {
+    members: {
+        Gandalf: 'Staff',
+        Frodo: 'Sting',
+        Sam: 'Lembas Bread',
+        Aragorn: 'Anduril',
+        Legolas: 'Bow of the Galadhrim',
+        Gimli: 'Walking Axe',
+        Pippin: 'Barrow Blades',
+        Merry: 'Barrow Blades',
+        Boromir: 'Horn of Gondor'
+    },
+    purpose: "To take the One Ring to Mordor, where it is to be \"cast into the fiery chasm from whence it came,\" in order for it to be destroyed and Sauron\'s power to come to an end.",
+    formed: '25 October, 3018',
+    dissolved: '26 February, 3019'
+}
+
+
+for (member in fellowshipOfTheRing.members){
+    if (member[0] !== 'A' && member[0]!=='E' && member[0]!== 'I' && member[0] !== 'O' && member[0] !=='U') {                     //THIS IS CASE SENSITIVE
+        console.log(`${member}'s name does not begin with a vowel`);
+    } else {
+        console.log(`${member}'s name does begin with a vowel`);
+    }
+}
+//if we use the || OR conditional - it would have checked if ANY of the conditions were true - AND && checks for ALL of them to be true
+
+//GOLD
+
+
+let fellowshipOfTheRing = {
+    members: {
+        Gandalf: 'Staff',
+        Frodo: 'Sting',
+        Sam: 'Lembas Bread',
+        Aragorn: 'Anduril',
+        Legolas: 'Bow of the Galadhrim',
+        Gimli: 'Walking Axe',
+        Pippin: 'Barrow Blades',
+        Merry: 'Barrow Blades',
+        Boromir: 'Horn of Gondor'
+    },
+    purpose: "To take the One Ring to Mordor, where it is to be \"cast into the fiery chasm from whence it came,\" in order for it to be destroyed and Sauron\'s power to come to an end.",
+    formed: '25 October, 3018',
+    dissolved: '26 February, 3019'
+}
+
+let arr=[];
+
+for (member in fellowshipOfTheRing.members){
+    if (member[0] !== 'A' && member[0]!=='E' && member[0]!== 'I' && member[0] !== 'O' && member[0] !=='U'){
+        arr.push(member);
+    }
+}
+console.log(arr);
+
+
